370a21d721839fe0d9f3fa0dc36c7eb036c3e04c
Ali Baris Oztekin <alibarisoztekin@gmail.com>
2017-05-18 11:55:15 -0700

    initial commit

---
+++ Wiivv Assignment-1/EtsyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EtsyTableViewCell.swift
+//  Wiivv Assignment-1
+//
+//  Created by Ali Barış Öztekin on 2017-05-18.
+//  Copyright © 2017 Ali Barış Öztekin. All rights reserved.
+//
+
+import UIKit
+
+class EtsyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
